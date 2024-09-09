6828d553f6c5b9ac23c6a4bdfe8728e8c6a789c0
cheongmok <asa4163dev@gmail.com>
2024-09-09 22:00:12 +0900

    Gold3_16236. 아기 상어 #3
    메모리 12216
    시간 72

--- Gold/16236/seokju.java
+++ Gold/16236/seokju.java
@@ -4,57 +4,45 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-// BOJ 16236 아기 상어
+// BOJ 16236 아기상어
 
 /*
- * NxN 크기의 공간에 물고기 M마리와 아기 상어 1마리
- * 아기 상어와 물고기는 모두 크기를 가지고 있고 자연수, 처음 아기 상어의 크기 2, 1초에 상하좌우로 인접한 한 칸씩 이동한다.
- * 자신의 크기보다 큰 물고기가 있는 칸은 지나갈 수 없고, 나머지 칸은 모두 지나갈 수 있다. 자신의 크기보다 작은 물고기만 먹을 수 있다.
- * 크기가 같은 물고기는 먹을 수 없지만 지나갈 수 있다.
- * 더 이상 먹을 수 있는 물고기가 공간에 없다면 아기 상어는 엄마 상어에게 도움을 요청
- * 먹을 수 있는 물고기가 1마리라면, 그 물고기를 먹으러 간다.
- * 1마리보다 많다면, 거리가 가장 가까운 물고기를 먹으러 간다.
- *  - 거리는 아기 상어가 있는 칸에서 물고기가 있는 칸으로 이동할 때, 지나야하는 칸의 개수의 최솟값이다.
- *  - 거리가 가까운 물고기가 많다면, 가장 위에 있는 물고기, 그러한 물고기가 여러마리라면, 가장 왼쪽에 있는 물고기를 먹는다.
- *  아기 상어의 이동은 1초
- *  아기 상어는 자신의 크기와 같은 수의 물고기를 먹을 때 마다 크기가 1 증가.
- *  아기 상어가 몇 초 동안 엄마 상어에게 도움을 요청하지 않고 물고기를 잡아 먹을 수 있는지 구하는 프로그램
- *  
- *  크기 2~20 0 빈칸 123456 물고기 크기 9 아기상어 위치
- *  
- *  1. 공간의 크기를 입력 받는다.
- *  2. 공간 정보를 입력받으면서 
- *  3. 물고기들의 정보를 저장해둔다. 
- *  4. 먹을 수 있는 물고기를 찾는다
- *   4-1. 한마리라면 그 물고기를 먹으러 간다.
- *   4-2. 여러마리라면 가장 가까운 물고기를 고른다.
- *   4-3. 가장 가까운 물고기가 많다면 가장 위, 가장 왼쪽에 있는 물고기를 먹는다.
- *  5. 먹을 수 있는 물고기가 없을 때 까지 반복
+NxN 크기의 공간에 물고기 M마리와 아기 상어 1마리가 있다.
+한 칸에는 물고기가 최대 1마리 존재한다.
+아기 상어와 물고기는 모두 크기를 가지고 있고, 이 크기는 자연수이다.
+가장 처음에 아기 상어의 크기는 2이고, 아기 상어는 1초에 상하좌우로 인접한 한 칸씩 이동한다.
+아기 상어는 자신의 크기보다 큰 물고기가 있는 칸은 지나갈 수 없고 나머지 칸은 모두 지나갈 수 있다.
+아기 상어는 자신의 크기보다 작은 물고기만 먹을 수 있다. 따라서, 크기가 같은 물고기는 먹을 수 없지만, 지나갈 수 있다.
+이동할지 결정하는 방법
+	- 더 이상 먹을 수 있는 물고기가 없다면 도움 요청
+	- 1마리라면 그 물고기를 먹으러 간다
+	- 1마리보다 많다면 거리가 가장 가까운 물고기를 먹으러 간다.
+		- 거리는 지나야하는 칸의 개수의 최솟값
+		- 거리가 가까운 물고기가 많다면, 가장 위에 있는 물고기, 그러한 물고기가 여러마리라면, 가장 왼쪽에 있는 물고기
+아기 상어의 이동은 1초 걸리고, 물고기를 먹는데 걸리는 시간은 없다고 가정
+아기 상어는 자신의 크기와 같은 수의 물고기를 먹을 때 마다 크기가 1증가
+공간의 상태가 주어졌을 때, 아기 상어가 몇 초 동안 엄마 상어에게 도움을 요청하지 않고 물고기를 잡아먹을 수 있는지 구하는 프로그램
+
+1. 첫째 줄에 공간의 크기 N 2~20
+2. 둘째 줄부터 공간의 상태. 0 빈칸 123456 물고기의 크기 9 아기 상어의 위치
+3. 아기상어가 먹이를 너비 우선으로 탐색한다.
+	3-1. 공간 밖이면 못움직인다.
+	3-2. 이미 지나간 공간이면 지나가지 않는다.
+	3-3. 아기상어보다 큰 물고기가 있다면 못지나간다.
+	3-4. 먹이를 찾으면 현재까지 탐색한 깊이가 이동시간이다.
+4. 탐색을 해도 먹이가 없으면 종료
  */
 
-public class BOJ16236 {
+public class BOJ16236_아기상어 {
 	static BufferedReader br;
 	static StringTokenizer st;
 
 	static int size;
-	static int[][] space;
-
-	static class Fish {
-		int row;
-		int col;
-		int size;
-
-		public Fish(int row, int col, int size) {
-			this.row = row;
-			this.col = col;
-			this.size = size;
-		}
-	}
+	static int[][] map;
 
 	static class Shark {
 		int row;
@@ -62,177 +50,143 @@ public class BOJ16236 {
 		int size;
 		int count;
 
-		public Shark(int row, int col, int size) {
+		public Shark(int row, int col, int size, int count) {
 			this.row = row;
 			this.col = col;
 			this.size = size;
-			this.count = 0;
+			this.count = count;
 		}
 
-		public void eatFish(Fish fish) {
+		public void eat() {
 			count++;
-			if (size == count) {
+			if (count == size) {
 				size++;
 				count = 0;
 			}
-			this.row = fish.row;
-			this.col = fish.col;
-			fish.size = 0;
-			space[this.row][this.col] = 0;
 		}
 	}
 
-	static class Position {
-		int row;
-		int col;
-		int length;
-
-		public Position(int row, int col, int length) {
-			this.row = row;
-			this.col = col;
-			this.length = length;
-		}
-	}
-
-	static Queue<Position> queue;
-
-	static List<Fish> fishes;
+	static final int SHARK = 9;
 	static Shark babyShark;
-	static int moveSecond;
+
+	static int elapsedTime;
 
 	public static void init() throws IOException {
-		targetFish = null;
-		moveSecond = 0;
-
+		// 1. 첫째 줄에 공간의 크기 N 2~20
 		size = Integer.parseInt(br.readLine().trim());
 
-		space = new int[size][size];
-		fishes = new ArrayList<>();
-		for (int rowIndex = 0; rowIndex < size; ++rowIndex) {
+		// 2. 둘째 줄부터 공간의 상태. 0 빈칸 123456 물고기의 크기 9 아기 상어의 위치
+		map = new int[size][size];
+		for (int row = 0; row < size; ++row) {
 			st = new StringTokenizer(br.readLine().trim());
-			for (int colIndex = 0; colIndex < size; ++colIndex) {
-				space[rowIndex][colIndex] = Integer.parseInt(st.nextToken());
-				if (space[rowIndex][colIndex] == 9) {
-					space[rowIndex][colIndex] = 0;
-					babyShark = new Shark(rowIndex, colIndex, 2);
-				} else if (space[rowIndex][colIndex] != 0) {
-					fishes.add(new Fish(rowIndex, colIndex, space[rowIndex][colIndex]));
+			for (int col = 0; col < size; ++col) {
+				map[row][col] = Integer.parseInt(st.nextToken());
+				if (map[row][col] == SHARK) {
+					babyShark = new Shark(row, col, 2, 0);
 				}
 			}
 		}
+		elapsedTime = 0;
 	}
 
-	static final int[] SHARK_DELTA_ROW = { 0, 0, 1, -1 };
-	static final int[] SHARK_DELTA_COL = { 1, -1, 0, 0 };
 	static boolean[][] visited;
+	static final int[] ROW_DELTA = { -1, 0, 0, 1 };
+	static final int[] COL_DELTA = { 0, -1, 1, 0 };
 
-	static class TargetFish {
-		Fish fish;
-		int length;
-
-		public TargetFish(Fish fish, int length) {
-			this.fish = fish;
-			this.length = length;
-		}
+	public static boolean canMove(Shark shark) {
+		return shark.row >= 0 && shark.row < size && shark.col >= 0 && shark.col < size;
 	}
 
-	static List<TargetFish> targetFishes;
-	static TargetFish targetFish;
-	static int minDist;
-	static int fishDist;
-
-	public static void findEatableFish() {
-		for (int fishIndex = 0; fishIndex < fishes.size(); ++fishIndex) {
-			if (fishes.get(fishIndex).size == 0) {
-				continue;
-			}
-			// 큰 물고기는 못잡아 먹는다
-			if (fishes.get(fishIndex).size >= babyShark.size) {
-				continue;
-			}
-			fishDist = 400;
-			if (canEat(fishes.get(fishIndex))) {
-				minDist = Math.min(minDist, fishDist);
-				targetFishes.add(new TargetFish(fishes.get(fishIndex), fishDist));
-			}
-		}
-		if (targetFishes.size() == 0) {
-			// 먹을 수 있는 물고기가 없음
-			targetFish = null;
-		} else if (targetFishes.size() == 1) {
-			// 한마리라면 그 물고기가 타겟
-			targetFish = targetFishes.get(0);
-		} else {
-			int minFishDist = targetFishes.get(0).length;
-			targetFish = targetFishes.get(0);
-			for (int fishIndex = 1; fishIndex < targetFishes.size(); ++fishIndex) {
-				if (minFishDist > targetFishes.get(fishIndex).length) {
-					minFishDist = targetFishes.get(fishIndex).length;
-					targetFish = targetFishes.get(fishIndex);
-				} else if (minFishDist == targetFishes.get(fishIndex).length) {
-					if (targetFishes.get(fishIndex).fish.row < targetFish.fish.row) {
-						targetFish = targetFishes.get(fishIndex);
-					} else if (targetFishes.get(fishIndex).fish.row == targetFish.fish.row) {
-						if (targetFishes.get(fishIndex).fish.col < targetFish.fish.col) {
-							targetFish = targetFishes.get(fishIndex);
-						}
-					}
-				}
-			}
-		}
+	public static boolean isVisited(Shark shark) {
+		return visited[shark.row][shark.col];
 	}
 
-	public static boolean canEat(Fish fish) {
-		boolean canEat = false;
+	public static boolean isThereBiggerFish(Shark shark) {
+		return map[shark.row][shark.col] != SHARK && map[shark.row][shark.col] > shark.size;
+	}
+
+	public static boolean isThereEatableFish(Shark shark) {
+		return map[shark.row][shark.col] != SHARK && map[shark.row][shark.col] != 0 && map[shark.row][shark.col] < shark.size;
+	}
+
+	public static boolean findEatableFish() {
 		visited = new boolean[size][size];
-
-		queue = new ArrayDeque<>();
-		queue.add(new Position(babyShark.row, babyShark.col, 0));
+		Queue<Shark> queue = new ArrayDeque<>();
+		queue.offer(babyShark);
 		visited[babyShark.row][babyShark.col] = true;
 
+		// 3. 아기상어가 먹이를 너비 우선으로 탐색한다.
+		int distance = 0;
+		int fishRow = -1;
+		int fishCol = -1;
 		while (!queue.isEmpty()) {
-			Position position = queue.poll();
-			int row = position.row;
-			int col = position.col;
-			int length = position.length;
-			if (row == fish.row && col == fish.col) {
-				canEat = true;
-				fishDist = length;
-				queue.clear();
-				break;
+			int queueSize = queue.size();
+			distance++;
+
+			while (--queueSize >= 0) {
+				Shark curShark = queue.poll();
+
+				for (int delta = 0; delta < ROW_DELTA.length; ++delta) {
+					Shark nextShark = new Shark(curShark.row + ROW_DELTA[delta], curShark.col + COL_DELTA[delta],
+							curShark.size, curShark.count);
+
+					// 3-1. 공간 밖이면 못움직인다.
+					if (!canMove(nextShark)) {
+						continue;
+					}
+					// 3-2. 이미 지나간 공간이면 지나가지 않는다.
+					if (isVisited(nextShark)) {
+						continue;
+					}
+					// 3-3. 아기상어보다 큰 물고기가 있다면 못지나간다.
+					if (isThereBiggerFish(nextShark)) {
+						continue;
+					}
+					// 3-4. 먹이를 찾으면 현재까지 탐색한 깊이가 이동시간이다.
+					if (isThereEatableFish(nextShark)) {
+						// babyShark = nextShark;
+						// babyShark.eat();
+						// map[babyShark.row][babyShark.col] = 0;
+						// elapsedTime += distance;
+						if (fishRow == -1) {
+							fishRow = nextShark.row;
+							fishCol = nextShark.col;
+						} else {
+							if (nextShark.row < fishRow) {
+								fishRow = nextShark.row;
+								fishCol = nextShark.col;
+							} else if (nextShark.row == fishRow) {
+								if (nextShark.col < fishCol) {
+									fishRow = nextShark.row;
+									fishCol = nextShark.col;
+								}
+							}
+						}
+						continue;
+					}
+					// 없다면 계속 탐색
+					queue.offer(nextShark);
+					visited[nextShark.row][nextShark.col] = true;
+				}
 			}
-
-			for (int delta = 0; delta < SHARK_DELTA_ROW.length; ++delta) {
-				int nextRow = row + SHARK_DELTA_ROW[delta];
-				int nextCol = col + SHARK_DELTA_COL[delta];
-
-				// 범위 체크
-				if (nextRow < 0 || nextRow >= size || nextCol < 0 || nextCol >= size) {
-					continue;
-				}
-				// 갈 수 있는 지 확인
-				if (visited[nextRow][nextCol]) {
-					continue;
-				}
-				if (space[nextRow][nextCol] > babyShark.size) {
-					continue;
-				}
-				visited[nextRow][nextCol] = true;
-				queue.add(new Position(nextRow, nextCol, length + 1));
+			// 3-4. 먹이를 찾으면 현재까지 탐색한 깊이가 이동시간이다.
+			if (fishRow != -1) {
+				babyShark = new Shark(fishRow, fishCol, babyShark.size, babyShark.count);
+				babyShark.eat();
+				map[fishRow][fishCol] = 0;
+				elapsedTime += distance;
+				return true;
 			}
 		}
-		return canEat;
+		return false;
 	}
 
 	public static void move() {
 		while (true) {
-			targetFishes = new ArrayList<>();
-			findEatableFish();
-			if (targetFish == null) {
+			if (!findEatableFish()) {
+				// 4. 탐색을 해도 먹이가 없으면 종료
+				System.out.println(elapsedTime);
 				return;
-			} else {
-				babyShark.eatFish(targetFish.fish);
-				moveSecond += targetFish.length;
 			}
 		}
 	}
@@ -242,6 +196,5 @@ public class BOJ16236 {
 
 		init();
 		move();
-		System.out.println(moveSecond);
 	}
 }
